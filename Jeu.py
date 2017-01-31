f0d762bb025607ea9aa6b81ff882de0ec82204d7
Unknown <esteban.milleret@gmail.com>
2017-01-31 09:47:10 +0100

    Affichage du Score

--- Jeu.py
+++ Jeu.py
@@ -19,9 +19,20 @@ fenetre = pygame.display.set_mode((700,700), RESIZABLE)
 # Création fond d'écran
 fond_e = pygame.image.load("Images/fond_cuisine.jpg").convert()
 
+# Création d'un objet 'Score'
+points = 0
+multiplicateur = 1
+score = Classes.Score(points, multiplicateur)
+
+# Création du texte du score
+font = pygame.font.Font(None, 24)
+
+points_text = font.render("Points : " + str(score.get_pts()), 1, (255,255,255))
+multiplicateur_text = font.render("Multiplicateur : " + str(score.get_mult()), 1, (255,255,255))
+
 # Charge l'image de la pastèque et définit sa vitesse
-speed = [2, 0]
-pasteque = Classes.Pasteque(0, 500, "Images/Pasteque.png", 27, 29, speed, 100, 5, 0)
+speed = [5, 0]
+pasteque = Classes.Pasteque(0, 305, "Images/pasteque200.png", 100, 100, speed, 100, 5, 0)
 
 # boucle infinie pour affichage permanent de la fenêtre
 while 1:
@@ -36,6 +47,8 @@ while 1:
     # On affiche les différentes images
     fenetre.blit(fond_e, (0,0))
     fenetre.blit(pasteque.get_img(), pasteque.get_rect())
+    fenetre.blit(points_text, (30,30))
+    fenetre.blit(multiplicateur_text, (30,50))
 
     #On refresh l'affichage
     pygame.display.flip()
