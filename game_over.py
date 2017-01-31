599a3042340d81fcd79b89efc77ef318e245c262
NIKOLNIKOV Kirill <nikolnik@hostname.iut2.upmf-grenoble.fr>
2017-01-31 16:18:49 +0100

    Game Over

--- game_over.py
+++ game_over.py
@@ -27,132 +27,6 @@ fenetre.blit(fond_e,(0,0))
 font = pygame.font.Font(None, 24)
 
 pygame.display.flip()
-def Credit():
-    fond_e = pygame.image.load("Images/fond_cuisine.jpg").convert()
-
-
-    # Création du texte du score
-    fenetre.blit(fond_e,(0,0))
-    font = pygame.font.Font(None, 24)
-
-    pygame.display.flip()
-
-    class Button:
-        def __init__(self):
-            self.main()
-
-        #Create a display
-        def display(self):
-            self.screen = fenetre
-
-        #Update the display and show the button
-        def update_display(self):
-            #Parameters:               surface,      color,       x,   y,   length, height, width,    text,      text_color
-            self.Button1.create_button(self.screen, (127,51,6), 50, 600, 250,    75,    0,        "Retour", (255,255,255))
-            pygame.display.flip()
-
-
-        #Run the loop
-        def main(self):
-            self.Button1 = Buttons.Button()
-            self.display()
-            while True:
-                self.update_display()
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                    elif event.type == MOUSEBUTTONDOWN:
-                        if self.Button1.pressed(pygame.mouse.get_pos()):
-                            menuMain()
-
-    def menu(): #procedure qui affiche le menu
-
-        # musique du menu
-        pygame.mixer.music.load("Transforyou.mp3")
-        pygame.mixer.music.play()
-
-        #création fond d'écran menu
-        fond_e = pygame.image.load("Images/fond_cuisine.jpg").convert()
-        fenetre.blit(fond_e,(0,0))              #affiche l'image "fond_e" aux coordonnées "(0,0)" de la fenêtre "fenetre"
-        pygame.display.flip()                   #rafraichit la fenêtre pour voir les changements
-
-
-        boutonJouer = Button()
-
-    while 1:
-        # Boucle sur les différents évènement reçut
-        for event in pygame.event.get():    # Ferme la fenetre si appuie sur la croix rouge
-            if event.type == QUIT:
-                sys.exit()
-        fenetre.blit(fond_e, (0,0))
-
-
-        #On refresh l'affichage
-        pygame.display.flip()
-
-
-        # Limite le nombre d'image par secondes
-        pygame.time.wait(10)
-        menu()
-
-
-
-
-def menuMain(): #procedure qui affiche le menu
-    fenetre  = pygame.display.set_mode((700,700), RESIZABLE)
-    pygame.display.set_caption('WATERMELON BREAKER')
-
-    class Button:
-        def __init__(self):
-            self.main()
-
-        #Create a display
-        def display(self):
-            self.screen = fenetre
-
-        #Update the display and show the button
-        def update_display(self):
-            #Parameters:               surface,      color,       x,   y,   length, height, width,    text,      text_color
-            self.Button1.create_button(self.screen, (127,51,6), 100, 200, 500,    75,    0,        "Jouer", (255,255,255))
-            pygame.display.flip()
-            self.Button2.create_button(self.screen,(127,51,6),100,400,500,75,0,"Credit",(255,255,255))
-
-        #Run the loop
-        def main(self):
-            self.Button1 = Buttons.Button()
-            self.Button2 = Buttons.Button()
-            self.display()
-            while True:
-                self.update_display()
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                    elif event.type == MOUSEBUTTONDOWN:
-                        if self.Button1.pressed(pygame.mouse.get_pos()):
-                            import Jeu
-                        if self.Button2.pressed(pygame.mouse.get_pos()):
-                            Credit()
-
-
-
-    #boucle infinie pour affichage permanent de la fenêtre
-    while 1:
-        #boucle sur les différents événements reçus
-        for event in pygame.event.get():
-            if event.type == QUIT:      #si l'utilisateur clique sur la croix
-                sys.exit()          #on ferme la fenêtre
-
-    # musique du menu
-    pygame.mixer.music.load("Transforyou.mp3")
-    pygame.mixer.music.play()
-
-    #création fond d'écran menu
-    fond_e = pygame.image.load("Images/fond_cuisine.jpg").convert()
-    fenetre.blit(fond_e,(0,0))              #affiche l'image "fond_e" aux coordonnées "(0,0)" de la fenêtre "fenetre"
-    pygame.display.flip()                   #rafraichit la fenêtre pour voir les changements
-
-
-    boutonJouer = Button()
 
 class Button:
     def __init__(self):
@@ -180,7 +54,7 @@ class Button:
                     pygame.quit()
                 elif event.type == MOUSEBUTTONDOWN:
                     if self.Button1.pressed(pygame.mouse.get_pos()):
-                        menuMain()
+                        import main
 
 def menu(): #procedure qui affiche le menu
 
